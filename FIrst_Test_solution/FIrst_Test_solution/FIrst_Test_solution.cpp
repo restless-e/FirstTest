cd89ee2c4a40c6dd14a9946e51266bc2040ae4fe
restless <restless.e@gmail.com>
2016-03-18 23:35:15 -0700

    solution

---
+++ FIrst_Test_solution/FIrst_Test_solution/FIrst_Test_solution.cpp
@@ -0,0 +1,24 @@
+// Test_1.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string input;
+
+	cout << "Input something" << endl;
+	cin >> input;
+
+	for (size_t i = 0; i < input.length(); i++)
+	{
+		cout << input[i] << " ";
+	}
+
+	system("pause");
+	return 0;
+}
+
